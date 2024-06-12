0a7349577e4e8fc74a3769d0763d9393a9f7011a
gosu <litongyi1999@163.com>
2024-06-12 11:20:37 +0800

    sql: create init

---
+++ migrations/20240612025601_init.sql
@@ -0,0 +1,39 @@
+-- postgresql initialization script
+
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    fullname VARCHAR(64) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    password_hash VARCHAR(64) NOT NULL,
+    create_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS email_index ON users(email);
+
+CREATE TYPE chat_type AS ENUM (
+    'single',
+    'group',
+    'private_channel',
+    'public_channel'
+);
+
+CREATE TABLE IF NOT EXISTS chats (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    type chat_type NOT NULL,
+    members bigint[] NOT NULL,
+    create_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    id BIGSERIAL PRIMARY KEY,
+    chat_id BIGINT NOT NULL,
+    sender_id BIGINT NOT NULL,
+    content TEXT NOT NULL,
+    images text[],
+    create_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX IF NOT EXISTS chat_id_create_at_index ON messages(chat_id, create_at DESC);
+
+CREATE INDEX IF NOT EXISTS sender_id_create_at_index ON messages(sender_id, create_at DESC);

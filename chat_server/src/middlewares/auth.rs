651a97db1de3bc0e6c113fea11824daa6dffbd86
gosu <litongyi1999@163.com>
2024-08-08 09:23:36 +0800

    test: verify token middleware

--- chat_server/src/middlewares/auth.rs
+++ chat_server/src/middlewares/auth.rs
@@ -39,3 +39,52 @@ pub async fn verify_token(
     parts.extensions.insert(user);
     next.run(Request::from_parts(parts, body)).await
 }
+
+#[cfg(test)]
+mod test {
+    use axum::{middleware::from_fn_with_state, routing::get, Router};
+    use http::Request;
+    use tower::ServiceExt;
+
+    use super::*;
+
+    #[axum::debug_handler]
+    async fn index_handler() -> &'static str {
+        "hello"
+    }
+
+    #[sqlx::test]
+    async fn verify_token_should_work(pool: sqlx::PgPool) -> anyhow::Result<()> {
+        let state = AppState::new_for_test(pool);
+        let router = Router::new()
+            .route("/", get(index_handler))
+            .layer(from_fn_with_state(state.clone(), verify_token));
+
+        let token = state.ek.sign(1)?;
+
+        // good token
+        let req = Request::builder()
+            .uri("/")
+            .header("Authorization", format!("Bearer {token}"))
+            .body(Body::empty());
+
+        let resp = router.clone().oneshot(req?).await?;
+        assert_eq!(resp.status(), StatusCode::OK);
+
+        // bad token
+        let req = Request::builder()
+            .uri("/")
+            .header("Authorization", "Bearer nothing")
+            .body(Body::empty());
+
+        let resp = router.clone().oneshot(req?).await?;
+        assert_eq!(resp.status(), StatusCode::FORBIDDEN);
+
+        // missing token
+        let req = Request::builder().uri("/").body(Body::empty());
+        let resp = router.clone().oneshot(req?).await?;
+        assert_eq!(resp.status(), StatusCode::UNAUTHORIZED);
+
+        Ok(())
+    }
+}
